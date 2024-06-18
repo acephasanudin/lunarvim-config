a6765744d294e70a58bad75cfc60a7600ae96b2b
Acep Hasanudin <acephasanudin24@gmail.com>
2024-06-18 14:32:46 +0000

    feat(dotnet): add DAP for dotnet development

--- config.lua
+++ config.lua
@@ -1,5 +1,4 @@
 ------------------------
-------------------------
 -- Treesitter
 ------------------------
 lvim.builtin.treesitter.ensure_installed = {
@@ -15,8 +14,6 @@ lvim.plugins = {
   "leoluz/nvim-dap-go",
   {'akinsho/toggleterm.nvim', version = "*", config = true},
 }
-
-
 
 ------------------------
 -- Formatting
@@ -46,17 +43,128 @@ local dap_ok, dapgo = pcall(require, "dap-go")
 if not dap_ok then
   return
 end
-
 dapgo.setup()
+
+local dap = require('dap')
+local dapui = require('dapui')
+
+dap.adapters.coreclr = {
+  type = 'executable',
+  command = '/usr/local/bin/netcoredbg/netcoredbg',
+  args = {'--interpreter=vscode'}
+}
+
+dap.configurations.cs = {
+  {
+    type = "coreclr",
+    name = "launch - netcoredbg",
+    request = "launch",
+    program = function()
+        return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+    end,
+  },
+}
+
+-- Normal mode key mappings
+lvim.keys.normal_mode["<F5>"] = "<Cmd>lua require'dap'.continue()<CR>"
+lvim.keys.normal_mode["<F10>"] = "<Cmd>lua require'dap'.step_over()<CR>"
+lvim.keys.normal_mode["<F11>"] = "<Cmd>lua require'dap'.step_into()<CR>"
+lvim.keys.normal_mode["<F12>"] = "<Cmd>lua require'dap'.step_out()<CR>"
+
+-- Leader key mappings
+lvim.keys.normal_mode["<Leader>n"] = "<Cmd>lua require'dap'.toggle_breakpoint()<CR>"
+lvim.keys.normal_mode["<Leader>N"] = "<Cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>"
+lvim.keys.normal_mode["<Leader>lp"] = "<Cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>"
+lvim.keys.normal_mode["<Leader>dr"] = "<Cmd>lua require'dap'.repl.open()<CR>"
+lvim.keys.normal_mode["<Leader>dl"] = "<Cmd>lua require'dap'.run_last()<CR>"
+
+dapui.setup({
+  icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
+  mappings = {
+    -- Use a table to apply multiple mappings
+    expand = { "<CR>", "<2-LeftMouse>" },
+    open = "o",
+    remove = "d",
+    edit = "e",
+    repl = "r",
+    toggle = "t",
+  },
+  -- Use this to override mappings for specific elements
+  element_mappings = {
+    -- Example:
+    -- stacks = {
+    --   open = "<CR>",
+    --   expand = "o",
+    -- }
+  },
+  expand_lines = vim.fn.has("nvim-0.7") == 1,
+  layouts = {
+    {
+      elements = {
+      -- Elements can be strings or table with id and size keys.
+        { id = "scopes", size = 0.25 },
+        "breakpoints",
+        "stacks",
+        "watches",
+      },
+      size = 40, -- 40 columns
+      position = "left",
+    },
+    {
+      elements = {
+        "repl",
+        "console",
+      },
+      size = 0.25, -- 25% of total lines
+      position = "bottom",
+    },
+  },
+  controls = {
+    -- Requires Neovim nightly (or 0.8 when released)
+    enabled = true,
+    -- Display controls in this element
+    element = "repl",
+    icons = {
+      pause = "",
+      play = "",
+      step_into = "",
+      step_over = "",
+      step_out = "",
+      step_back = "",
+      run_last = "↻",
+      terminate = "□",
+    },
+  },
+  floating = {
+    max_height = nil, -- These can be integers or a float between 0 and 1.
+    max_width = nil, -- Floats will be treated as percentage of your screen.
+    border = "single", -- Border style. Can be "single", "double" or "rounded"
+    mappings = {
+      close = { "q", "<Esc>" },
+    },
+  },
+  windows = { indent = 1 },
+  render = {
+    max_type_length = nil, -- Can be integer or nil.
+    max_value_lines = 100, -- Can be integer or nil.
+  }
+})
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
 
 ------------------------
 -- LSP
 ------------------------
 local lsp_manager = require "lvim.lsp.manager"
 
-------------------------
--- LSP
-------------------------
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "gopls" })
 
 lsp_manager.setup("golangci_lint_ls", {
@@ -115,6 +223,7 @@ gopher.setup {
     iferr = "iferr",
   },
 }
+
 
 lvim.transparent_window = true
 
